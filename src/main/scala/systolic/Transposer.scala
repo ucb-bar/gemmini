09de885c2a956318e44f39e10a9656900983a40c
Vighnesh Iyer <vighnesh.iyer@gmail.com>
2019-04-26 15:12:05 -0700

    working on systolic transposer

--- src/main/scala/systolic/Transposer.scala
+++ src/main/scala/systolic/Transposer.scala
@@ -3,13 +3,92 @@ package systolic
 import chisel3._
 import chisel3.util._
 
-class Transposer[T <: Data](val dim: Int, datatype: T) extends Module {
+trait Transposer[T <: Data] extends Module {
+  def dim: Int
+  def dataType: T
   val io = IO(new Bundle {
-    val inRow = Flipped(Decoupled(Vec(dim, datatype)))
-    val outCol = Decoupled(Vec(dim, datatype))
+    val inRow = Flipped(Decoupled(Vec(dim, dataType)))
+    val outCol = Decoupled(Vec(dim, dataType))
   })
+}
 
-  val regArray = Seq.fill(dim, dim)(Reg(datatype))
+class PipelinedTransposer[T <: Data](val dim: Int, val dataType: T) extends Transposer[T] {
+  require(isPow2(dim))
+  val regArray = Seq.fill(dim, dim)(Reg(dataType))
+  val regArrayT = regArray.transpose
+  val sMoveUp :: sMoveLeft :: Nil = Enum(2)
+  val state = RegInit(sMoveUp)
+  val leftCounter = RegInit(0.U(log2Ceil(dim+1).W)) //(io.inRow.fire() && state === sMoveLeft, dim+1)
+  val upCounter = RegInit(0.U(log2Ceil(dim+1).W)) //Counter(io.inRow.fire() && state === sMoveUp, dim+1)
+
+  io.outCol.valid := 0.U
+  io.inRow.ready := 0.U
+  switch(state) {
+    is(sMoveUp) {
+      io.inRow.ready := upCounter <= dim.U
+      io.outCol.valid := leftCounter > 0.U
+      when(io.inRow.fire()) {
+        upCounter := upCounter + 1.U
+      }
+      when(upCounter === (dim-1).U) {
+        state := sMoveLeft
+        leftCounter := 0.U
+      }
+    }
+    is(sMoveLeft) {
+      io.inRow.ready := leftCounter <= dim.U // TODO: this is naive
+      io.outCol.valid := upCounter > 0.U
+      when(leftCounter === (dim-1).U) {
+        state := sMoveUp
+      }
+      when(io.inRow.fire()) {
+        leftCounter := leftCounter + 1.U
+        upCounter := 0.U
+      }
+      when(io.outCol.fire()) {
+        upCounter := upCounter - 1.U
+      }
+    }
+  }
+
+  // Propagate input from bottom row to top row systolically in the move up phase
+  // TODO: need to iterate over columns to connect Chisel values of type T
+  // Should be able to operate directly on the Vec, but Seq and Vec don't mix (try Array?)
+  for (colIdx <- 0 until dim) {
+    regArray.foldRight(io.inRow.bits(colIdx)) {
+      case (regRow, prevReg) =>
+        when (state === sMoveUp) {
+          regRow(colIdx) := prevReg
+        }
+        regRow(colIdx)
+    }
+  }
+
+  // Propagate input from right side to left side systolically in the move left phase
+  for (rowIdx <- 0 until dim) {
+    regArrayT.foldRight(io.inRow.bits(rowIdx)) {
+      case (regCol, prevReg) =>
+        when (state === sMoveLeft) {
+          regCol(rowIdx) := prevReg
+        }
+        regCol(rowIdx)
+    }
+  }
+
+  // Pull from the left side or the top side based on the state
+  for (idx <- 0 until dim) {
+    when (state === sMoveUp) {
+      io.outCol.bits(idx) := regArray(0)(idx)
+    }.elsewhen(state === sMoveLeft) {
+      io.outCol.bits(idx) := regArrayT(0)(idx)
+    }.otherwise {
+      io.outCol.bits(idx) := DontCare
+    }
+  }
+}
+
+class NaiveTransposer[T <: Data](val dim: Int, val dataType: T) extends Transposer[T] {
+  val regArray = Seq.fill(dim, dim)(Reg(dataType))
   val regArrayT = regArray.transpose
   // state = 0 => filling regArray row-wise, state = 1 => draining regArray column-wise
   val state = RegInit(0.U(1.W))
--- src/test/scala/systolic/TransposerUnitTest.scala
+++ src/test/scala/systolic/TransposerUnitTest.scala
@@ -4,30 +4,49 @@ import chisel3._
 import chisel3.iotesters.{ChiselFlatSpec, PeekPokeTester}
 import systolic.TestUtils.{Matrix, consecutive, identity, rand}
 
-class TransposerTester[T <: Bits](c: Transposer[T], mats: Seq[Matrix[Int]]) extends PeekPokeTester(c) {
-  reset()
-  for (mat <- mats) {
-    val matT = mat.transpose
-    for (row <- mat) {
-      poke(c.io.inRow.valid, 1)
-      row.zipWithIndex.foreach { case(elem, i) =>
-        poke(c.io.inRow.bits(i), elem)
-      }
-      step(1)
-    }
-    poke(c.io.inRow.valid, 0)
-    expect(c.io.inRow.ready, 0)
+// TODO: replace Transposer type parameter with wildcard (with LUB of Data)
+class TransposerTester[+C <: Transposer[UInt]](c: C, mats: Seq[Matrix[Int]], dim: Int)
+  extends PeekPokeTester[C](c) {
+  val rowsToPush = mats.flatten
+  val expectedCols = mats.map(_.transpose).flatten
+  val actualCols = collection.mutable.Buffer.empty[Seq[Int]]
 
-    for (col <- 0 until c.dim) {
-      poke(c.io.outCol.ready, 1)
-      expect(c.io.outCol.valid, 1)
-      (0 until c.dim).foreach { i =>
-        expect(c.io.outCol.bits(i), matT(col)(i))
+  reset()
+  poke(c.io.outCol.ready, 1)
+
+  rowsToPush.foreach { row =>
+    poke(c.io.inRow.valid, 1)
+    println(s"Pushing row: $row")
+    row.zipWithIndex.foreach { case(elem, i) =>
+      poke(c.io.inRow.bits(i), elem)
+    }
+    if (peek(c.io.inRow.ready) == 1) {
+      if (peek(c.io.outCol.valid) == 1) {
+        actualCols += peek(c.io.outCol.bits).map(_.intValue)
       }
       step(1)
     }
-    poke(c.io.outCol.ready, 0)
+    else {
+      while(peek(c.io.inRow.ready) != 1) {
+        if (peek(c.io.outCol.valid) == 1) {
+          actualCols += peek(c.io.outCol.bits).map(_.intValue)
+        }
+        step(1)
+      }
+      step(1)
+    }
   }
+
+  poke(c.io.inRow.valid, 0)
+  while(peek(c.io.outCol.valid) != 0) {
+    actualCols += peek(c.io.outCol.bits).map(_.intValue)
+    step(1)
+  }
+
+  Predef.println(rowsToPush)
+  Predef.println(expectedCols)
+  Predef.println(actualCols)
+  assert(expectedCols == actualCols)
 }
 
 class TransposerUnitTest extends ChiselFlatSpec {
@@ -35,9 +54,20 @@ class TransposerUnitTest extends ChiselFlatSpec {
     "--backend-name", "treadle",
     "--generate-vcd-output", "on",
     "--target-dir", "test_run_dir/transposer",
-    "--top-name", "transposer"
+    "--top-name"
   )
-  chisel3.iotesters.Driver.execute(testerArgs, () => new Transposer(4, UInt(8.W))) {
-    c => new TransposerTester(c, Seq(identity(4), rand(4, 255), consecutive(4)))
-  } should be (true)
+  val dim = 2
+  behavior of "NaiveTransposer"
+  it should "transpose one matrix" in {
+    chisel3.iotesters.Driver.execute(testerArgs :+ "naive_transposer", () => new NaiveTransposer(dim, UInt(8.W))) {
+      c => new TransposerTester(c, Seq(identity(dim), rand(dim, 255), consecutive(dim)), dim)
+    } should be (true)
+  }
+
+  behavior of "PipelinedTransposer"
+  it should "transpose one matrix" in {
+    chisel3.iotesters.Driver.execute(testerArgs :+ "pipe_transposer", () => new PipelinedTransposer(dim, UInt(8.W))) {
+      c => new TransposerTester(c, Seq(consecutive(dim)), dim)
+    } should be (true)
+  }
 }
