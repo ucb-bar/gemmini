66e8c3879a1fd06d10d767c4e88377f0c46bbdd2
Hasan Genc <hngenc@berkeley.edu>
2019-07-25 11:22:39 -0700

    Fixed DSEConfig7

--- src/main/scala/systolic/DSEConfigs.scala
+++ src/main/scala/systolic/DSEConfigs.scala
@@ -43,7 +43,7 @@ object DSEConfigs{
     sp_width = 8*32, acc_rows = 16*1024*8 / (32*32), headerFileName = "systolic_params_dse5.h")
   val fullyCombinationalConfig = baseConfig.copy(tileRows = 16, tileColumns = 16, meshRows = 1, meshColumns = 1, headerFileName = "systolic_params_dse6.h")
   val moreMemoryConfig = baseConfig.copy(sp_bank_entries = 256*1024*8 / (4*16*8),
-    acc_rows = 64*1024*8/(16*32), shifter_banks = 16, headerFileName = "systolic_params_dse7.h") // 256kB
+    acc_rows = 64*1024*8/(16*32), headerFileName = "systolic_params_dse7.h") // 256kB
   val lessBanksConfig = baseConfig.copy(sp_banks = 1, sp_bank_entries = 64*1024*8/(1*16*8), headerFileName = "systolic_params_dse8.h")
   val narrowerBusConfig = baseConfig.copy(dma_maxbytes = 64, dma_buswidth = 64, headerFileName = "systolic_params_dse10.h")
 }
