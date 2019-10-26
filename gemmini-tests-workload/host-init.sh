0bc2021ef14600918e045dcb409babdd4216beb8
alonamid <alonamid@eecs.berkeley.edu>
2019-10-26 04:20:54 +0000

    workload test stuff

---
+++ gemmini-tests-workload/host-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script will run on the host from the workload directory
+# (e.g. workloads/example-fed) every time the workload is built.
+# It is recommended to call into something like a makefile because
+# this script may be called multiple times.
+echo "Building gemmini-rocc-tests benchmark"
+cd gemmini-rocc-tests
+autoconf
+mkdir -p build && cd build
+../configure
+make TARGET=riscv64-unknown-linux-gnu-
+cp -r ./* ../../overlay/root/
