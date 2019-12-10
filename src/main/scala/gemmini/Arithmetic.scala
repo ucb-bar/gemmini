94051bcd6b85b239281b6735785492cf50a47566
Hasan Genc <hngenc@berkeley.edu>
2019-12-09 22:39:44 -0800

    Bug fixes for floats

--- src/main/scala/gemmini/Arithmetic.scala
+++ src/main/scala/gemmini/Arithmetic.scala
@@ -86,14 +86,14 @@ object Arithmetic {
   implicit object FloatArithmetic extends Arithmetic[Float] {
     override implicit def cast(self: Float): ArithmeticOps[Float] = new ArithmeticOps(self) {
       override def mac(m1: Float, m2: Float): Float = {
-        val m1_rec = recFNFromFN(self.recExpWidth, self.recSigWidth, m1.bits)
-        val m2_rec = recFNFromFN(self.recExpWidth, self.recSigWidth, m2.bits)
+        val m1_rec = recFNFromFN(self.expWidth, self.sigWidth, m1.bits)
+        val m2_rec = recFNFromFN(self.expWidth, self.sigWidth, m2.bits)
         val self_rec = recFNFromFN(self.expWidth, self.sigWidth, self.bits)
 
-        val muladder = Module(new MulAddRecFN(self.recExpWidth, self.recSigWidth))
+        val muladder = Module(new MulAddRecFN(self.expWidth, self.sigWidth))
 
         muladder.io.op := 0.U
-        muladder.io.roundingMode := consts.round_near_maxMag
+        muladder.io.roundingMode := consts.round_near_even // consts.round_near_maxMag
         muladder.io.detectTininess := consts.tininess_afterRounding
 
         muladder.io.a := m1_rec
@@ -101,26 +101,26 @@ object Arithmetic {
         muladder.io.c := self_rec
 
         val out = Wire(Float(self.expWidth, self.sigWidth))
-        out.bits := fNFromRecFN(self.recExpWidth, self.recSigWidth, muladder.io.out)
+        out.bits := fNFromRecFN(self.expWidth, self.sigWidth, muladder.io.out)
         out
       }
 
       override def +(t: Float): Float = {
-        val t_rec = recFNFromFN(self.recExpWidth, self.recSigWidth, t.bits)
+        val t_rec = recFNFromFN(self.expWidth, self.sigWidth, t.bits)
         val self_rec = recFNFromFN(self.expWidth, self.sigWidth, self.bits)
 
-        val in_to_rec_fn = Module(new INToRecFN(1, self.recExpWidth, self.recSigWidth))
+        val in_to_rec_fn = Module(new INToRecFN(1, self.expWidth, self.sigWidth))
         in_to_rec_fn.io.signedIn := false.B
         in_to_rec_fn.io.in := 1.U
-        in_to_rec_fn.io.roundingMode := consts.round_near_maxMag
+        in_to_rec_fn.io.roundingMode := consts.round_near_even // consts.round_near_maxMag
         in_to_rec_fn.io.detectTininess := consts.tininess_afterRounding
 
         val one_rec = in_to_rec_fn.io.out
 
-        val muladder = Module(new MulAddRecFN(self.recExpWidth, self.recSigWidth))
+        val muladder = Module(new MulAddRecFN(self.expWidth, self.sigWidth))
 
         muladder.io.op := 0.U
-        muladder.io.roundingMode := consts.round_near_maxMag
+        muladder.io.roundingMode := consts.round_near_even // consts.round_near_maxMag
         muladder.io.detectTininess := consts.tininess_afterRounding
 
         muladder.io.a := t_rec
@@ -128,17 +128,45 @@ object Arithmetic {
         muladder.io.c := self_rec
 
         val result = Wire(Float(self.expWidth, self.sigWidth))
-        result.bits := fNFromRecFN(self.recExpWidth, self.recSigWidth, muladder.io.out)
+        result.bits := fNFromRecFN(self.expWidth, self.sigWidth, muladder.io.out)
         result
       }
 
       override def >>(u: UInt): Float = {
-        val raw = rawFloatFromFN(self.recExpWidth, self.recSigWidth, self.bits)
+        // Recode self
+        val self_rec = recFNFromFN(self.expWidth, self.sigWidth, self.bits)
+
+        // Get 2^(-u) as a recoded float
+        val shift_exp = Wire(UInt(self.expWidth.W))
+        shift_exp := self.bias.U - u
+        val shift_fn = Cat(0.U(1.W), shift_exp, 0.U((self.sigWidth-1).W))
+        val shift_rec = recFNFromFN(self.expWidth, self.sigWidth, shift_fn)
+
+        // Multiply self and 2^(-u)
+        val muladder = Module(new MulAddRecFN(self.expWidth, self.sigWidth))
+
+        muladder.io.op := 0.U
+        muladder.io.roundingMode := consts.round_near_even // consts.round_near_maxMag
+        muladder.io.detectTininess := consts.tininess_afterRounding
+
+        muladder.io.a := self_rec
+        muladder.io.b := shift_rec
+        muladder.io.c := 0.U
+
+        val result = Wire(Float(self.expWidth, self.sigWidth))
+        result.bits := fNFromRecFN(self.expWidth, self.sigWidth, muladder.io.out)
+        result
+
+        /*
+        val raw = rawFloatFromFN(self.expWidth, self.sigWidth, self.bits)
 
         val shifted_raw = WireInit(raw)
-        shifted_raw.sExp := raw.sExp - u.asSInt()
 
-        val raw_to_rec_fn_converter = Module(new RoundRawFNToRecFN(self.recExpWidth, self.recSigWidth, options = 0)) // TODO add correct options here so that efficiency may be improved
+        when (!raw.isZero) {
+          shifted_raw.sExp := raw.sExp - u.asSInt()
+        }
+
+        val raw_to_rec_fn_converter = Module(new RoundRawFNToRecFN(self.expWidth, self.sigWidth, options = 0)) // TODO add correct options here so that efficiency may be improved
 
         raw_to_rec_fn_converter.io.invalidExc := false.B
         raw_to_rec_fn_converter.io.infiniteExc := false.B
@@ -149,39 +177,40 @@ object Arithmetic {
         raw_to_rec_fn_converter.io.detectTininess := consts.tininess_afterRounding
 
         val result = Wire(Float(self.expWidth, self.sigWidth))
-        result.bits := fNFromRecFN(self.recExpWidth, self.recSigWidth, raw_to_rec_fn_converter.io.out)
+        result.bits := fNFromRecFN(self.expWidth, self.sigWidth, raw_to_rec_fn_converter.io.out)
         result
+        */
       }
 
       override def withWidthOf(t: Float): Float = {
-        val self_rec = recFNFromFN(self.recExpWidth, self.recSigWidth, self.bits)
+        val self_rec = recFNFromFN(self.expWidth, self.sigWidth, self.bits)
 
-        val resizer = Module(new RecFNToRecFN(self.recExpWidth, self.recSigWidth, t.recExpWidth, t.recSigWidth))
+        val resizer = Module(new RecFNToRecFN(self.expWidth, self.sigWidth, t.expWidth, t.sigWidth))
         resizer.io.in := self_rec
-        resizer.io.roundingMode := consts.round_near_maxMag
+        resizer.io.roundingMode := consts.round_near_even // consts.round_near_maxMag
         resizer.io.detectTininess := consts.tininess_afterRounding
 
         val result = Wire(Float(t.expWidth, t.sigWidth))
-        result.bits := fNFromRecFN(t.recExpWidth, t.recSigWidth, resizer.io.out)
+        result.bits := fNFromRecFN(t.expWidth, t.sigWidth, resizer.io.out)
         result
       }
 
       override def clippedToWidthOf(t: Float): Float = {
         // TODO check for overflow. Right now, we just assume that overflow doesn't happen
-        val self_rec = recFNFromFN(self.recExpWidth, self.recSigWidth, self.bits)
+        val self_rec = recFNFromFN(self.expWidth, self.sigWidth, self.bits)
 
-        val resizer = Module(new RecFNToRecFN(self.recExpWidth, self.recSigWidth, t.recExpWidth, t.recSigWidth))
+        val resizer = Module(new RecFNToRecFN(self.expWidth, self.sigWidth, t.expWidth, t.sigWidth))
         resizer.io.in := self_rec
-        resizer.io.roundingMode := consts.round_near_maxMag
+        resizer.io.roundingMode := consts.round_near_even // consts.round_near_maxMag
         resizer.io.detectTininess := consts.tininess_afterRounding
 
         val result = Wire(Float(t.expWidth, t.sigWidth))
-        result.bits := fNFromRecFN(t.recExpWidth, t.recSigWidth, resizer.io.out)
+        result.bits := fNFromRecFN(t.expWidth, t.sigWidth, resizer.io.out)
         result
       }
 
       override def relu: Float = {
-        val raw = rawFloatFromFN(self.recExpWidth, self.recSigWidth, self.bits)
+        val raw = rawFloatFromFN(self.expWidth, self.sigWidth, self.bits)
 
         val result = Wire(Float(self.expWidth, self.sigWidth))
         result.bits := Mux(!raw.isZero && raw.sign, 0.U, self.bits)
@@ -189,22 +218,58 @@ object Arithmetic {
       }
 
       override def relu6(shift: UInt): Float = {
+        // Get a constant 6 as a float
+        val in_to_rec_fn = Module(new INToRecFN(log2Up(6+1), self.expWidth, self.sigWidth))
+        in_to_rec_fn.io.signedIn := false.B
+        in_to_rec_fn.io.in := 6.U
+        in_to_rec_fn.io.roundingMode := consts.round_near_even // consts.round_near_maxMag
+        in_to_rec_fn.io.detectTininess := consts.tininess_afterRounding
+
+        val six_rec = in_to_rec_fn.io.out
+
+        // Get 2^shift as a float
+        val shift_exp = Wire(UInt(self.expWidth.W))
+        shift_exp := self.bias.U + shift
+        val shift_fn = Cat(0.U(1.W), shift_exp, 0.U((self.sigWidth-1).W))
+        val shift_rec = recFNFromFN(self.expWidth, self.sigWidth, shift_fn)
+
+        // Get 6*(2^shift) as a float
+        val muladder = Module(new MulAddRecFN(self.expWidth, self.sigWidth))
+
+        muladder.io.op := 0.U
+        muladder.io.roundingMode := consts.round_near_even // consts.round_near_maxMag
+        muladder.io.detectTininess := consts.tininess_afterRounding
+
+        muladder.io.a := six_rec
+        muladder.io.b := shift_rec
+        muladder.io.c := 0.U
+
+        val shifted_rec = muladder.io.out
+
+        /*
         val six_raw = rawFloatFromIN(signedIn = false.B, in = 6.U(3.W))
 
         val shifted_raw = WireInit(six_raw)
-        shifted_raw.sExp := six_raw.sExp + shift.asSInt()
 
-        val raw_to_rec_fn_converter = Module(new RoundRawFNToRecFN(self.recExpWidth, self.recSigWidth, options = 0)) // TODO add correct options here so that efficiency may be improved
+        when (!six_raw.isZero) {
+          shifted_raw.sExp := six_raw.sExp + shift.asSInt()
+        }
+
+        val raw_to_rec_fn_converter = Module(new RoundRawFNToRecFN(self.expWidth, self.sigWidth, options = 0)) // TODO add correct options here so that efficiency may be improved
         raw_to_rec_fn_converter.io.in := shifted_raw
         raw_to_rec_fn_converter.io.roundingMode := consts.round_near_maxMag
         raw_to_rec_fn_converter.io.detectTininess := consts.tininess_afterRounding
+        raw_to_rec_fn_converter.io.invalidExc := false.B
+        raw_to_rec_fn_converter.io.infiniteExc := false.B
 
         val shifted_rec = raw_to_rec_fn_converter.io.out
+        */
 
-        val self_rec = recFNFromFN(self.recExpWidth, self.recSigWidth, self.bits)
-        val self_raw = rawFloatFromFN(self.recExpWidth, self.recSigWidth, self.bits)
+        // Now, compare self and 6*(2^shift) to calculate the activation function
+        val self_rec = recFNFromFN(self.expWidth, self.sigWidth, self.bits)
+        val self_raw = rawFloatFromFN(self.expWidth, self.sigWidth, self.bits)
 
-        val comparer = Module(new CompareRecFN(self.recExpWidth, self.recSigWidth))
+        val comparer = Module(new CompareRecFN(self.expWidth, self.sigWidth))
         comparer.io.a := self_rec
         comparer.io.b := shifted_rec
         comparer.io.signaling := false.B
@@ -215,7 +280,7 @@ object Arithmetic {
           Mux(larger_than_six, shifted_rec, self_rec))
 
         val result = Wire(Float(self.expWidth, self.sigWidth))
-        result.bits := fNFromRecFN(self.recExpWidth, self.recSigWidth, self_rec)
+        result.bits := fNFromRecFN(self.expWidth, self.sigWidth, self_rec)
         result
       }
     }
@@ -223,8 +288,7 @@ object Arithmetic {
 }
 
 case class Float(expWidth: Int, sigWidth: Int) extends Bundle {
-  val bits = UInt((expWidth + sigWidth + 1).W)
+  val bits = UInt((expWidth + sigWidth).W)
 
-  val recExpWidth = expWidth + 1
-  val recSigWidth = sigWidth
+  val bias: Int = (1 << (expWidth-1)) - 1
 }
