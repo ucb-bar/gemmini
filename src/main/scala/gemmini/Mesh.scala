c0c792bcb9f8ecb75444d08bdf95d6bd43f3aaaf
Hasan Genc <hngenc@berkeley.edu>
2020-01-22 15:16:08 -0800

    Added initial code to support Mesh blackbox

--- src/main/scala/gemmini/Mesh.scala
+++ src/main/scala/gemmini/Mesh.scala
@@ -3,6 +3,7 @@ package gemmini
 
 import chisel3._
 import chisel3.util._
+import chisel3.experimental._
 
 /**
   * A Grid is a 2D array of Tile modules with registers in between each tile and
@@ -13,6 +14,65 @@ import chisel3.util._
   * @param meshRows
   * @param meshColumns
   */
+class Mesh[T <: Data : Arithmetic](inputType: T, outputType: T, accType: T,
+                                   df: Dataflow.Value, pe_latency: Int,
+                                   val tileRows: Int, val tileColumns: Int,
+                                   val meshRows: Int, val meshColumns: Int) extends Module {
+  val io = IO(new Bundle {
+    val in_a   = Input(Vec(meshRows, Vec(tileRows, inputType)))
+    val in_b   = Input(Vec(meshColumns, Vec(tileColumns, inputType)))
+    val in_d   = Input(Vec(meshColumns, Vec(tileColumns, inputType)))
+    val in_control   = Input(Vec(meshColumns, Vec(tileColumns, new PEControl(accType))))
+    val out_b  = Output(Vec(meshColumns, Vec(tileColumns, outputType)))
+    val out_c  = Output(Vec(meshColumns, Vec(tileColumns, outputType)))
+
+    val in_valid = Input(Vec(meshColumns, Vec(tileColumns, Bool())))
+    val out_valid = Output(Vec(meshColumns, Vec(tileColumns, Bool())))
+  })
+
+  val bb = Module(new MeshBlackBox(inputType=inputType, outputType=outputType,
+    rows=meshRows*tileRows, columns=meshColumns*tileColumns))
+
+  bb.io.in_a := io.in_a.asUInt()
+  bb.io.in_b := io.in_b.asUInt()
+  bb.io.in_d := io.in_d.asUInt()
+
+  bb.io.in_dataflow := Cat(io.in_control.flatten.map(_.dataflow))
+  bb.io.in_propagate := Cat(io.in_control.flatten.map(_.propagate))
+
+  io.out_b := bb.io.out_b.asTypeOf(io.out_b)
+  io.out_c := bb.io.out_c.asTypeOf(io.out_c)
+
+  bb.io.in_valid := io.in_valid.asUInt()
+  io.out_valid := bb.io.out_valid.asTypeOf(io.out_valid)
+}
+
+class MeshBlackBox[T <: Data : Arithmetic](inputType: T, outputType: T,
+                                           val rows: Int, val columns: Int) extends BlackBox(Map(
+  "ROWS" -> s"$rows",
+  "COLS" -> s"$columns",
+  "INPUT_BITWIDTH" -> s"${inputType.getWidth}",
+  "OUTPUT_BITWIDTH" -> s"${outputType.getWidth}"
+)) {
+  val io = IO(new Bundle {
+    val in_a   = Input(UInt((rows * inputType.getWidth).W))
+    val in_b   = Input(UInt((columns * inputType.getWidth).W))
+    val in_d   = Input(UInt((columns * inputType.getWidth).W))
+
+    val in_dataflow = Input(UInt(columns.W))
+    val in_propagate = Input(UInt(columns.W))
+
+    val out_b  = Output(UInt((columns * outputType.getWidth).W))
+    val out_c  = Output(UInt((columns * outputType.getWidth).W))
+
+    val in_valid = Input(UInt(columns.W))
+    val out_valid = Output(UInt(columns.W))
+  })
+
+  setResource("/MeshBlackBox.v")
+}
+
+/*
 class Mesh[T <: Data : Arithmetic](inputType: T, outputType: T, accType: T,
                       df: Dataflow.Value, pe_latency: Int,
                       val tileRows: Int, val tileColumns: Int,
@@ -93,3 +153,4 @@ class Mesh[T <: Data : Arithmetic](inputType: T, outputType: T, accType: T,
     v := RegNext(tile.io.out_valid)
   }
 }
+*/
