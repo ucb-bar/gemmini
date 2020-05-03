dcb5b71707689ffc5bac60f12d746d048bf7b79a
Hasan Genc <hngenc@berkeley.edu>
2020-05-03 02:32:26 -0700

    Made sure operand addresses can span across banks in ExecuteController.scala

--- src/main/scala/gemmini/ExecuteController.scala
+++ src/main/scala/gemmini/ExecuteController.scala
@@ -154,13 +154,31 @@ class ExecuteController[T <: Data, U <: Data](xLen: Int, tagWidth: Int, config: 
   val matmul_in_progress = mesh.io.tags_in_progress.map(_.rob_id.valid).reduce(_ || _)
 
   // SRAM scratchpad
-  val dataAbank = a_address_rs1.sp_bank()
-  val dataBbank = b_address_rs2.sp_bank()
-  val dataDbank = d_address_rs1.sp_bank()
+  // Fire counters which resolve same-bank accesses
+  val a_fire_counter = Reg(UInt(log2Up(block_size).W))
+  val b_fire_counter = Reg(UInt(log2Up(block_size).W))
+  val d_fire_counter = Reg(UInt(log2Up(block_size).W))
 
-  val dataABankAcc = a_address_rs1.acc_bank()
-  val dataBBankAcc = b_address_rs2.acc_bank()
-  val dataDBankAcc = d_address_rs1.acc_bank()
+  // These "*_fire_started" variables are only needed for 2x2 systolic arrays
+  val a_fire_started = RegInit(false.B)
+  val d_fire_started = RegInit(false.B)
+  val b_fire_started = RegInit(false.B)
+
+  // "A" stride variables
+  val a_addr_offset = Reg(UInt((16 + log2Up(block_size)).W))
+  val a_addr_stride = Reg(UInt(16.W))
+
+  val a_address = a_address_rs1 + a_fire_counter
+  val b_address = b_address_rs2 + b_fire_counter
+  val d_address = d_address_rs1 + (block_size.U - 1.U - d_fire_counter)
+
+  val dataAbank = a_address.sp_bank()
+  val dataBbank = b_address.sp_bank()
+  val dataDbank = d_address.sp_bank()
+
+  val dataABankAcc = a_address.acc_bank()
+  val dataBBankAcc = b_address.acc_bank()
+  val dataDBankAcc = d_address.acc_bank()
 
   val a_read_from_acc = a_address_rs1.is_acc_addr
   val b_read_from_acc = b_address_rs2.is_acc_addr
@@ -180,24 +198,10 @@ class ExecuteController[T <: Data, U <: Data](xLen: Int, tagWidth: Int, config: 
   val performing_single_mul = WireInit(perform_single_mul && control_state === compute)
   val performing_mul_pre = WireInit(perform_mul_pre && control_state === compute)
 
-  // Fire counters which resolve same-bank accesses
-  val a_fire_counter = Reg(UInt(log2Up(block_size).W))
-  val b_fire_counter = Reg(UInt(log2Up(block_size).W))
-  val d_fire_counter = Reg(UInt(log2Up(block_size).W))
-
-  // These "*_fire_started" variables are only needed for 2x2 systolic arrays
-  val a_fire_started = RegInit(false.B)
-  val d_fire_started = RegInit(false.B)
-  val b_fire_started = RegInit(false.B)
-
   // These variables determine whether or not the row that is currently being read should be completely padded with 0
   val a_row_is_not_all_zeros = a_fire_counter < a_rows
   val b_row_is_not_all_zeros = b_fire_counter < b_rows
   val d_row_is_not_all_zeros = block_size.U - 1.U - d_fire_counter < d_rows
-
-  // "A" stride variables
-  val a_addr_offset = Reg(UInt((16 + log2Up(block_size)).W))
-  val a_addr_stride = Reg(UInt(16.W))
 
   def same_bank(addr1: LocalAddr, addr2: LocalAddr, start_inputting1: Bool, start_inputting2: Bool): Bool = {
     val addr1_read_from_acc = addr1.is_acc_addr
@@ -217,9 +221,9 @@ class ExecuteController[T <: Data, U <: Data](xLen: Int, tagWidth: Int, config: 
   case class Operand(addr: LocalAddr, start_inputting: Bool, counter: UInt, started: Bool, priority: Int) {
     val done = counter === 0.U && started
   }
-  val a_operand = Operand(a_address_rs1, start_inputting_a, a_fire_counter, a_fire_started, 0)
-  val b_operand = Operand(b_address_rs2, start_inputting_b, b_fire_counter, b_fire_started, 1)
-  val d_operand = Operand(d_address_rs1, start_inputting_d, d_fire_counter, d_fire_started, 2)
+  val a_operand = Operand(a_address, start_inputting_a, a_fire_counter, a_fire_started, 0)
+  val b_operand = Operand(b_address, start_inputting_b, b_fire_counter, b_fire_started, 1)
+  val d_operand = Operand(d_address, start_inputting_d, d_fire_counter, d_fire_started, 2)
   val operands = Seq(a_operand, b_operand, d_operand)
 
   val Seq(a_valid, b_valid, d_valid) = operands.map { case Operand(addr, start_inputting, counter, started, priority) =>
@@ -306,9 +310,9 @@ class ExecuteController[T <: Data, U <: Data](xLen: Int, tagWidth: Int, config: 
 
     io.srams.read(i).req.valid := read_a || read_b || read_d
     io.srams.read(i).req.bits.fromDMA := false.B
-    io.srams.read(i).req.bits.addr := MuxCase(a_address_rs1.sp_row() +  a_addr_offset, // a_fire_counter,
-      Seq(read_b -> (b_address_rs2.sp_row() + b_fire_counter),
-        read_d -> (d_address_rs1.sp_row() + block_size.U - 1.U - d_fire_counter)))
+    io.srams.read(i).req.bits.addr := MuxCase(a_address.sp_row(),
+      Seq(read_b -> b_address.sp_row(),
+        read_d -> d_address.sp_row()))
 
     io.srams.read(i).resp.ready := true.B
   }
@@ -331,9 +335,9 @@ class ExecuteController[T <: Data, U <: Data](xLen: Int, tagWidth: Int, config: 
     io.acc.read(i).req.bits.act := activation
     io.acc.read(i).req.bits.fromDMA := false.B
 
-    io.acc.read(i).req.bits.addr := MuxCase(a_address_rs1.acc_row() + a_addr_offset, // a_fire_counter,
-      Seq(read_b_from_acc -> (b_address_rs2.acc_row() + b_fire_counter),
-        read_d_from_acc -> (d_address_rs1.acc_row() + block_size.U - 1.U - d_fire_counter)))
+    io.acc.read(i).req.bits.addr := MuxCase(a_address.acc_row(),
+      Seq(read_b_from_acc -> b_address.acc_row(),
+        read_d_from_acc -> d_address.acc_row()))
 
     io.acc.read(i).resp.ready := true.B
   }
