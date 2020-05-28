fd9d9016ae1fd685fe518fb739e9ef4860523d70
Hasan Genc <hngenc@berkeley.edu>
2020-05-28 07:05:23 +0000

    Updated FireSim test workloads

--- software/overlay/root/run-tests-full.sh
+++ software/overlay/root/run-tests-full.sh
@@ -2,60 +2,84 @@
 
 echo "*****************TEST RESULTS*************" > test_output.txt
 echo "=========tiled_matmul========="
+echo "=========tiled_matmul_cpu-linux=========" >> test_output.txt
+/root/bareMetalC/tiled_matmul_cpu-linux >> test_output.txt
 echo "=========tiled_matmul_os-linux=========" >> test_output.txt
 /root/bareMetalC/tiled_matmul_os-linux >> test_output.txt
 echo "=========tiled_matmul_ws-linux=========" >> test_output.txt
 /root/bareMetalC/tiled_matmul_ws-linux >> test_output.txt
-echo "=========tiled_matmul_cpu-linux=========" >> test_output.txt
-/root/bareMetalC/tiled_matmul_cpu-linux >> test_output.txt
+
+echo "=========conv========="
+echo "=========conv-linux=========" >> test_output.txt
+/root/bareMetalC/conv-linux >> test_output.txt
+echo "=========conv_with_pool-linux=========" >> test_output.txt
+/root/bareMetalC/conv_with_pool-linux >> test_output.txt
+
 echo "========mobilenet========="
-echo "========mobilenet OS=========" >> test_output.txt
-/root/mobilenet/test os >> test_output.txt
-echo "========mobilenet WS=========" >> test_output.txt
-/root/mobilenet/test ws >> test_output.txt
 echo "========mobilenet CPU=========" >> test_output.txt
-/root/mobilenet/test cpu >> test_output.txt
+/root/imagenet/mobilenet-linux cpu matmul >> test_output.txt
+echo "========mobilenet conv CPU=========" >> test_output.txt
+/root/imagenet/mobilenet-linux cpu conv >> test_output.txt
+echo "========mobilenet OS=========" >> test_output.txt
+/root/imagenet/mobilenet-linux os matmul >> test_output.txt
+echo "========mobilenet WS=========" >> test_output.txt
+/root/imagenet/mobilenet-linux ws matmul >> test_output.txt
+echo "========mobilenet conv WS=========" >> test_output.txt
+/root/imagenet/mobilenet-linux ws conv >> test_output.txt
+
 echo "========ResNet50========="
-echo "========ResNet50-16 OS=========" >> test_output.txt
-/root/resnet/resnet50-16 os >> test_output.txt
-echo "========ResNet50-16 WS=========" >> test_output.txt
-/root/resnet/resnet50-16 ws >> test_output.txt
 echo "========ResNet50 CPU=========" >> test_output.txt
-/root/resnet/resnet50-16 cpu >> test_output.txt
-echo "========ResNet152========="
-echo "========ResNet152-16 OS=========" >> test_output.txt
-/root/resnet/resnet152-16 os >> test_output.txt
-echo "========ResNet152-16 WS=========" >> test_output.txt
-/root/resnet/resnet152-16 ws >> test_output.txt
-echo "========ResNet152 CPU=========" >> test_output.txt
-/root/resnet/resnet152-16 cpu >> test_output.txt
-echo "========Gemmini Library Test 1========="
-echo "========Gemmini Library Test 1 OS=========" >> test_output.txt
-/root/gemmini_library/test1 os >> test_output.txt
-echo "========Gemmini Library Test 1 WS=========" >> test_output.txt
-/root/gemmini_library/test1 ws >> test_output.txt
-echo "========Gemmini Library Test 1 CPU=========" >> test_output.txt
-/root/gemmini_library/test1 cpu >> test_output.txt
-echo "========Gemmini Library Test 2========="
-echo "========Gemmini Library Test 2 OS=========" >> test_output.txt
-/root/gemmini_library/test2 os >> test_output.txt
-echo "========Gemmini Library Test 2 WS=========" >> test_output.txt
-/root/gemmini_library/test2 ws >> test_output.txt
-echo "========Gemmini Library Test 2 CPU=========" >> test_output.txt
-/root/gemmini_library/test2 cpu >> test_output.txt
-echo "========Gemmini Library Test 3========="
-echo "========Gemmini Library Test 3 OS=========" >> test_output.txt
-/root/gemmini_library/test3 os >> test_output.txt
-echo "========Gemmini Library Test 3 WS=========" >> test_output.txt
-/root/gemmini_library/test3 ws >> test_output.txt
-echo "========Gemmini Library Test 3 CPU=========" >> test_output.txt
-/root/gemmini_library/test3 cpu >> test_output.txt
-echo "========Gemmini Library Test 4========="
-echo "========Gemmini Library Test 4 OS=========" >> test_output.txt
-/root/gemmini_library/test4 os >> test_output.txt
-echo "========Gemmini Library Test 4 WS=========" >> test_output.txt
-/root/gemmini_library/test4 ws >> test_output.txt
-echo "========Gemmini Library Test 4 CPU=========" >> test_output.txt
-/root/gemmini_library/test4 cpu >> test_output.txt
+/root/imagenet/resnet50-linux cpu matmul >> test_output.txt
+echo "========ResNet50 conv CPU=========" >> test_output.txt
+/root/imagenet/resnet50-linux cpu conv >> test_output.txt
+echo "========ResNet50 OS=========" >> test_output.txt
+/root/imagenet/resnet50-linux os matmul >> test_output.txt
+echo "========ResNet50 WS=========" >> test_output.txt
+/root/imagenet/resnet50-linux ws matmul >> test_output.txt
+echo "========ResNet50 conv WS=========" >> test_output.txt
+/root/imagenet/resnet50-linux ws conv >> test_output.txt
+
+echo "========MLP 1========="
+echo "========MLP 1 OS=========" >> test_output.txt
+/root/mlps/mlp1-linux os >> test_output.txt
+echo "========MLP 1 WS=========" >> test_output.txt
+/root/mlps/mlp1-linux ws >> test_output.txt
+echo "========MLP 2========="
+echo "========MLP 2 OS=========" >> test_output.txt
+/root/mlps/mlp2-linux os >> test_output.txt
+echo "========MLP 2 WS=========" >> test_output.txt
+/root/mlps/mlp2-linux ws >> test_output.txt
+echo "========MLP 3========="
+echo "========MLP 3 OS=========" >> test_output.txt
+/root/mlps/mlp3-linux os >> test_output.txt
+echo "========MLP 3 WS=========" >> test_output.txt
+/root/mlps/mlp3-linux ws >> test_output.txt
+echo "========MLP 4========="
+echo "========MLP 4 OS=========" >> test_output.txt
+/root/mlps/mlp4-linux os >> test_output.txt
+echo "========MLP 4 WS=========" >> test_output.txt
+/root/mlps/mlp4-linux ws >> test_output.txt
+
+echo "========MLP 1 (32)========="
+echo "========MLP 1 (32) OS=========" >> test_output.txt
+/root/mlps/mlp1_32-linux os >> test_output.txt
+echo "========MLP 1 (32) WS=========" >> test_output.txt
+/root/mlps/mlp1_32-linux ws >> test_output.txt
+echo "========MLP 2 (32)========="
+echo "========MLP 2 (32) OS=========" >> test_output.txt
+/root/mlps/mlp2_32-linux os >> test_output.txt
+echo "========MLP 2 (32) WS=========" >> test_output.txt
+/root/mlps/mlp2_32-linux ws >> test_output.txt
+echo "========MLP 3 (32)========="
+echo "========MLP 3 (32) OS=========" >> test_output.txt
+/root/mlps/mlp3_32-linux os >> test_output.txt
+echo "========MLP 3 (32) WS=========" >> test_output.txt
+/root/mlps/mlp3_32-linux ws >> test_output.txt
+echo "========MLP 4 (32)========="
+echo "========MLP 4 (32) OS=========" >> test_output.txt
+/root/mlps/mlp4_32-linux os >> test_output.txt
+echo "========MLP 4 (32) WS=========" >> test_output.txt
+/root/mlps/mlp4_32-linux ws >> test_output.txt
+
 cat test_output.txt
 poweroff -f
