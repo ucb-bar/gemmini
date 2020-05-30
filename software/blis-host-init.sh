309635548b7609dce733d1a3b15f7d37e3473caa
alonamid <alonamid@eecs.berkeley.edu>
2020-05-30 09:14:13 -0700

    blis workload scripts

--- software/blis-host-init.sh
+++ software/blis-host-init.sh
@@ -7,7 +7,8 @@
 echo "Building Blis"
 cd blis
 # configure to statically build for riscv64
-./configure CC=riscv64-unknown-linux-gnu-gcc CFLAGS=-static LDFLAGS=-static --disable-threading --disable-pba-pools --disable-sba-pools --disable-shared riscv64
+#./configure CC=riscv64-unknown-linux-gnu-gcc CFLAGS=-static LDFLAGS=-static --disable-threading --disable-pba-pools --disable-sba-pools --disable-shared riscv64
+./configure CC=riscv64-unknown-linux-gnu-gcc CFLAGS=-static LDFLAGS=-static --disable-threading --disable-pba-pools --disable-sba-pools --disable-shared gemmini
 # build library
 make clean
 make -j8
