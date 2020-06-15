13600ac19f9bc3e22135c70e7878e396de9d92a2
Seah <54855793+SeahK@users.noreply.github.com>
2020-06-14 19:02:15 -0700

    perf counter updates

--- src/main/scala/gemmini/Controller.scala
+++ src/main/scala/gemmini/Controller.scala
@@ -269,14 +269,13 @@ class GemminiModule[T <: Data: Arithmetic, U <: Data]
   FpgaDebug(st_ex_cycles)
   FpgaDebug(ld_st_ex_cycles)
 
-  PerfCounter(ld_cycles, "ld_cycles_cnt", "cycles where only load-controller is busy")
-  PerfCounter(st_cycles, "st_cycles_cnt", "cycles where only store-controller is busy")
-  PerfCounter(ex_cycles, "ex_cycles_cnt", "cycles where only execute-controller is busy")
-  PerfCounter(ld_st_cycles, "ld_st_cycles_cnt", "cycles where only load-store-controller is busy")
-  PerfCounter(ld_ex_cycles, "ld_ex_cycles_cnt", "cycles where only load-execute-controller is busy")
-  PerfCounter(st_ex_cycles, "st_ex_cycles_cnt", "cycles where only store-execute-controller is busy")
-  PerfCounter(ld_st_ex_cycles, "ld_st_ex_cycles_cnt", "cycles where only load-store-execute-controller is busy")
-
+  PerfCounter(incr_ld_cycles, "ld_cycles_cnt", "cycles where only load-controller is busy")
+  PerfCounter(incr_st_cycles, "st_cycles_cnt", "cycles where only store-controller is busy")
+  PerfCounter(incr_ex_cycles, "ex_cycles_cnt", "cycles where only execute-controller is busy")
+  PerfCounter(incr_ld_st_cycles, "ld_st_cycles_cnt", "cycles where only load-store-controller is busy")
+  PerfCounter(incr_ld_ex_cycles, "ld_ex_cycles_cnt", "cycles where only load-execute-controller is busy")
+  PerfCounter(incr_st_ex_cycles, "st_ex_cycles_cnt", "cycles where only store-execute-controller is busy")
+  PerfCounter(incr_ld_st_ex_cycles, "ld_st_ex_cycles_cnt", "cycles where only load-store-execute-controller is busy")
 
   // Issue commands to controllers
   // TODO we combinationally couple cmd.ready and cmd.valid signals here
