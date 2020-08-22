4cd66ae4797f7f86ebdac95f7bf00c2a1ea98197
alonamid <alonamid@eecs.berkeley.edu>
2020-08-22 01:41:24 -0700

    clean up blis-host-init

--- software/blis-host-init.sh
+++ software/blis-host-init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # This script will run on the host from the workload directory
 # (e.g. workloads/example-fed) every time the workload is built.
 # It is recommended to call into something like a makefile because
@@ -9,8 +11,10 @@ cd blis
 # configure to statically build for riscv64
 #./configure CC=riscv64-unknown-linux-gnu-gcc CFLAGS=-static LDFLAGS=-static --disable-threading --disable-pba-pools --disable-sba-pools --disable-shared riscv64
 ./configure CC=riscv64-unknown-linux-gnu-gcc CFLAGS=-static LDFLAGS=-static --disable-threading --disable-pba-pools --disable-sba-pools --disable-shared gemmini
-# build library
+# clean up state
+rm -rf ../overlay/root/blis/
 make clean
+# build library
 make -j8
 # build tests
 echo "Building Blis Tests"
@@ -18,14 +22,12 @@ make testsuite-bin
 make blastest-bin
 # copy to linux overlay
 echo "Copying Blis and tests to overlay"
-rm -rf ../overlay/root/blis/
 mkdir -p ../overlay/root/blis
 echo '#!/usr/bin/env bash' > ../overlay/root/blis/make-check.sh
 echo `` >> ../overlay/root/blis/make-check.sh
 make -n check >> ../overlay/root/blis/make-check.sh
 chmod 775 ../overlay/root/blis/make-check.sh
 cp test_libblis.x ../overlay/root/blis 
-cp make-check.sh ../overlay/root/blis 
 cp -r obj/ ../overlay/root/blis/obj
 cp -r lib/ ../overlay/root/blis/lib
 cp -r include/ ../overlay/root/blis/include
