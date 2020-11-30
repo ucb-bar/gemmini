04b90a0206b657c995132215d0e6f0b41054399f
Jerry Zhao <jerryz123@berkeley.edu>
2020-11-30 10:59:13 -0800

    Run RTL tests in FAST mode

--- .circleci/run-tests-rtl.sh
+++ .circleci/run-tests-rtl.sh
@@ -10,7 +10,7 @@ TOOLS_DIR=$LOCAL_ESP_DIR
 PATH=$PATH:$LOCAL_ESP_DIR/bin
 
 cd $LOCAL_CHIPYARD_DIR/generators/gemmini/software/gemmini-rocc-tests
-./build.sh
+CFLAGS=-DFAST ./build.sh
 
 cd build
 make test-baremetal-bareMetalC RUNNER="'make -C $LOCAL_CHIPYARD_DIR/sims/verilator/ CONFIG=GemminiRocketConfig run-binary-hex BINARY='"
