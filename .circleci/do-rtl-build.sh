70517c52f2d36c0fc1370b3b9836297646a70289
abejgonzalez <abe.j.gonza@gmail.com>
2020-12-11 11:32:33 -0800

    Update CI for new Chipyard

--- .circleci/do-rtl-build.sh
+++ .circleci/do-rtl-build.sh
@@ -23,4 +23,4 @@ export LD_LIBRARY_PATH=$LD_LIB_DIR
 export PATH=$LOCAL_VERILATOR_DIR/bin:$PATH
 export VERILATOR_ROOT=$LOCAL_VERILATOR_DIR
 export COURSIER_CACHE=$LOCAL_WORK_DIR/.coursier-cache
-make -j$LOCAL_MAKE_NPROC -C $LOCAL_SIM_DIR VERILATOR_OPT_FLAGS="-O0 -OG" JAVA_ARGS="-Xmx2500M -Xss8M -Dsbt.ivy.home=$LOCAL_CHIPYARD_DIR/.ivy2 -Dsbt.supershell=false -Dsbt.global.base=$LOCAL_CHIPYARD_DIR/.sbt -Dsbt.boot.directory=$LOCAL_CHIPYARD_DIR/.sbt/boot" CONFIG=GemminiRocketConfig
+make -j$LOCAL_MAKE_NPROC -C $LOCAL_SIM_DIR VERILATOR_OPT_FLAGS="-O0 -OG" JAVA_OPTS="-Xmx2500M -Xss8M" SBT_OPTS="-Dsbt.ivy.home=$LOCAL_CHIPYARD_DIR/.ivy2 -Dsbt.supershell=false -Dsbt.global.base=$LOCAL_CHIPYARD_DIR/.sbt -Dsbt.boot.directory=$LOCAL_CHIPYARD_DIR/.sbt/boot" CONFIG=GemminiRocketConfig
