c73b9fc8a80adaccdf9f380803be8b3aa348906c
Jerry Zhao <jerryz123@berkeley.edu>
2021-02-12 15:16:51 -0800

    Use a 2-entry queue instead

--- src/main/scala/gemmini/Controller.scala
+++ src/main/scala/gemmini/Controller.scala
@@ -197,7 +197,7 @@ class GemminiModule[T <: Data: Arithmetic, U <: Data, V <: Data]
   val (loop_cmd, loop_matmul_unroller_busy) = LoopMatmul(raw_cmd, rob.io.ld_utilization, rob.io.st_utilization, rob.io.ex_utilization,
     meshRows*tileRows, coreMaxAddrBits, rob_entries, 4, 12, 2, sp_banks * sp_bank_entries, acc_banks * acc_bank_entries,
     inputType.getWidth, accType.getWidth, dma_maxbytes)
-  val unrolled_cmd = Queue(loop_cmd, 1, pipe=true)
+  val unrolled_cmd = Queue(loop_cmd)
   unrolled_cmd.ready := false.B
 
   // val cmd_decompressor = Module(new InstDecompressor(rob_entries))
