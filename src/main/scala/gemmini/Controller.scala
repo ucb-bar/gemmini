2b3b0c3b3eeb1ea9ed17312c221def53e8f0a27f
Hasan Genc <hngenc@berkeley.edu>
2021-02-18 16:00:34 -0500

    Comment out interrupt assertion

--- src/main/scala/gemmini/Controller.scala
+++ src/main/scala/gemmini/Controller.scala
@@ -387,7 +387,7 @@ class GemminiModule[T <: Data: Arithmetic, U <: Data, V <: Data]
 
   rob.io.solitary_preload := ex_controller.io.solitary_preload
 
-  assert(!io.interrupt, "Interrupt handlers have not been written yet")
+  // assert(!io.interrupt, "Interrupt handlers have not been written yet")
 
   // Cycle counters
   val ld_cycles = RegInit(0.U(34.W))
