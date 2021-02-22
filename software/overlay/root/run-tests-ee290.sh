e3c877fc61ca3f8f94384de3826ca204f2310434
Abraham Gonzalez <abe.gonzalez@berkeley.edu>
2021-02-22 02:56:53 +0000

    Bump rocc-tests for CIFAR fix | Don't run MLP1

--- software/overlay/root/run-tests-ee290.sh
+++ software/overlay/root/run-tests-ee290.sh
@@ -7,8 +7,8 @@ echo "*****************TEST RESULTS*************"
 #/root/ee290/very_large_matmul-linux
 #echo "=========CIFAR DNN========="
 #/root/ee290/cifar_quant-linux
-echo "========Multi-Level Perceptron 1========="
-/root/mlps/mlp1-linux
+#echo "========Multi-Level Perceptron 1========="
+#/root/mlps/mlp1-linux
 echo "========Multi-Level Perceptron 2========="
 /root/mlps/mlp2-linux
 echo "========MobileNet========="
