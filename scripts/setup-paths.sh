db676c223797ef0aea9b93c7c5cf00c00ab8b381
Hasan Genc <hngenc@berkeley.edu>
2021-10-28 16:24:04 -0700

    fix setup script

--- scripts/setup-paths.sh
+++ scripts/setup-paths.sh
@@ -4,18 +4,18 @@ if [ ! -d configs ]; then
     mkdir configs/
 fi
 
-if [ ! -f configs/Configs.scala ]; then
+if [ ! -f configs/GemminiDefaultConfigs.scala ]; then
     ln -s $PWD/src/main/scala/gemmini/Configs.scala configs/GemminiDefaultConfigs.scala
 fi
 
 if [ ! -f configs/GemminiCustomConfigs.scala ]; then
-    ln -s $PWD/src/main/scala/gemmini/GemminiCustomConfigs.scala configs/GemminiCustomConfigs.scala
+    ln -s $PWD/src/main/scala/gemmini/CustomConfigs.scala configs/GemminiCustomConfigs.scala
 fi
 
 if [ ! -f configs/CPUConfigs.scala ]; then
-    ln -s $PWD/src/main/scala/gemmini/CustomCPUConfigs.scala configs/CPUConfigs..scala
+    ln -s $PWD/src/main/scala/gemmini/CustomCPUConfigs.scala configs/CPUConfigs.scala
 fi
 
-if [ ! -f configs/L2Configs.scala ]; then
-fi
+# if [ ! -f configs/L2Configs.scala ]; then
+# fi
 
