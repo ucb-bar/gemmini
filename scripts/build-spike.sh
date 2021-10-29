b7d1284a00fa5723fd8fed895492b4afadcdc2ab
Hasan Genc <hngenc@berkeley.edu>
2021-10-28 17:42:24 -0700

    pipe stdout as well

--- scripts/build-spike.sh
+++ scripts/build-spike.sh
@@ -4,7 +4,7 @@ export GEMMINI_ONLY_GENERATE_GEMMINI_H=1
 
 cd ../../sims/verilator/
 echo Generating new gemmini_params.h file...
-make CONFIG=CustomGemminiSoCConfig 2> build.log
+make CONFIG=CustomGemminiSoCConfig &> build.log
 
 cp software/gemmini-rocc-tests/include/gemmini_params.h ../../toolchains/esp-tools/riscv-isa-sim/gemmini/gemmini_params.h
 cd ../../toolchains/esp-tools/riscv-isa-sim/build
