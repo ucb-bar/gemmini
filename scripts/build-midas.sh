338c15a56afdeb953ebe69ca42bedc1389b82243
Hasan Genc <hngenc@berkeley.edu>
2021-10-28 18:57:36 -0700

    build midas

---
+++ scripts/build-midas.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$1" == "--help" ]; then
+    echo usage: $0 DRAM_CONTROLLER_MODEL
+    echo " " DRAM_CONTROLLER_MODEL: Either DDR3FRFCFS or DDR3FRFCFSLLC4MB or DDR3FCFS or DDR3FCFSLLC4MB
+    echo "   " FRFCFS is "first ready, first come, first serve"
+    echo "   " FCFS is "first come, first serve"
+elif [ "$1" == "" ]; then
+    echo DRAM model must be provided
+fi
+
+cd ../../sims/firesim/sim
+make verilator TARGET_CONFIG=${1}_WithDefaultFireSimBridges_WithFireSimConfigTweaks_chipyard.CustomGemminiSoCConfig
+
