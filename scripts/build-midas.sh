1ad3a3977461b0f8e9185a1b60182d4fe87cca61
Hasan Genc <hngenc@berkeley.edu>
2021-10-28 21:07:07 -0700

    remove nonexistent config

--- scripts/build-midas.sh
+++ scripts/build-midas.sh
@@ -2,9 +2,9 @@
 
 if [ "$1" == "--help" ]; then
     echo usage: $0 DRAM_CONTROLLER_MODEL
-    echo " " DRAM_CONTROLLER_MODEL: Either DDR3FRFCFS or DDR3FRFCFSLLC4MB or DDR3FCFS or DDR3FCFSLLC4MB
-    echo "   " FRFCFS is "first ready, first come, first serve"
+    echo " " DRAM_CONTROLLER_MODEL: Either DDR3FCFS or DDR3FRFCFS or DDR3FRFCFSLLC4MB
     echo "   " FCFS is "first come, first serve"
+    echo "   " FRFCFS is "first ready, first come, first serve"
     exit
 elif [ "$1" == "" ]; then
     echo DRAM model must be provided
