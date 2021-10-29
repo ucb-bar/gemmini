8dee35c9136f6bff9e27c1b22ab47a74b8db000c
Hasan Genc <hngenc@berkeley.edu>
2021-10-28 20:16:55 -0700

    add exit

--- scripts/build-midas.sh
+++ scripts/build-midas.sh
@@ -5,6 +5,7 @@ if [ "$1" == "--help" ]; then
     echo " " DRAM_CONTROLLER_MODEL: Either DDR3FRFCFS or DDR3FRFCFSLLC4MB or DDR3FCFS or DDR3FCFSLLC4MB
     echo "   " FRFCFS is "first ready, first come, first serve"
     echo "   " FCFS is "first come, first serve"
+    exit
 elif [ "$1" == "" ]; then
     echo DRAM model must be provided
 fi
