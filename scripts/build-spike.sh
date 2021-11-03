8e58acffb3c72e548b9da53b22d8ad364e3e04ca
Hasan Genc <hngenc@berkeley.edu>
2021-11-03 14:45:35 -0700

    fix build_spike with cd -

--- scripts/build-spike.sh
+++ scripts/build-spike.sh
@@ -6,6 +6,7 @@ cd ../../sims/verilator/
 echo Generating new gemmini_params.h file...
 make CONFIG=CustomGemminiSoCConfig &> build.log
 
+cd -
 cp software/gemmini-rocc-tests/include/gemmini_params.h ../../toolchains/esp-tools/riscv-isa-sim/gemmini/gemmini_params.h
 cd ../../toolchains/esp-tools/riscv-isa-sim/build
 make && make install
