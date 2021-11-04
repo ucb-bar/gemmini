bed1194c13149d3c899f8283d55506895334064e
Hasan Genc <hngenc@berkeley.edu>
2021-11-04 12:37:04 -0700

    make verilog

--- scripts/build-spike.sh
+++ scripts/build-spike.sh
@@ -4,7 +4,7 @@ export GEMMINI_ONLY_GENERATE_GEMMINI_H=1
 
 cd ../../sims/verilator/
 echo Generating new gemmini_params.h file...
-make CONFIG=CustomGemminiSoCConfig &> build.log
+make verilog CONFIG=CustomGemminiSoCConfig &> build.log
 
 cd -
 cp software/gemmini-rocc-tests/include/gemmini_params.h ../../toolchains/esp-tools/riscv-isa-sim/gemmini/gemmini_params.h
