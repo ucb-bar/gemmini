e1461ac4129b099e51ed0bcc2e3812bd1d5c9e86
Hasan Genc <hngenc@berkeley.edu>
2021-11-04 14:01:38 -0700

    tut

--- tutorial/checkpoint.sh
+++ tutorial/checkpoint.sh
@@ -6,11 +6,15 @@ if [ $checkpoint == "build-verilator" ]; then
     build=simulator-chipyard-CustomGemminiSoCConfig-Inference
     rtl=generated-src-inference
 
+    rm -rf /root/chipyard/sims/verilator/generated-src
+
     cp /root/builds/$build /root/chipyard/sims/verilator/simulator-chipyard-CustomGemminiSoCConfig
     cp -r /root/builds/$rtl /root/chipyard/sims/verilator/generated-src
 elif [ $checkpoint == "build-complex" ]; then
     build=simulator-chipyard-CustomGemminiSoCConfig-Complex
     rtl=generated-src-complex
+
+    rm -rf /root/chipyard/sims/verilator/generated-src
 
     cp /root/builds/$build /root/chipyard/sims/verilator/simulator-chipyard-CustomGemminiSoCConfig
     cp -r /root/builds/$rtl /root/chipyard/sims/verilator/generated-src
