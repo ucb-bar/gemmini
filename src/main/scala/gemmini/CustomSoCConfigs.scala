b68cd5a3da42fb8e05f4ef546778f65212c5ae62
Hasan Genc <hngenc@berkeley.edu>
2021-11-04 12:21:25 -0700

    commit

--- src/main/scala/gemmini/CustomSoCConfigs.scala
+++ src/main/scala/gemmini/CustomSoCConfigs.scala
@@ -5,13 +5,18 @@ import freechips.rocketchip.config.{Config}
 
 class CustomGemminiSoCConfig extends Config(
   new gemmini.GemminiCustomConfig ++
+
+  // Set your custom L2 configs
   new freechips.rocketchip.subsystem.WithInclusiveCache(
     nBanks = 1,
     nWays = 8,
     capacityKB = 512,
     outerLatencyCycles = 40
   ) ++
+
+  // Set the number of CPUs you want to create
   new chipyard.CustomGemmminiCPUConfigs.CustomCPU(1) ++
+
   new chipyard.config.AbstractConfig
 )
 */
