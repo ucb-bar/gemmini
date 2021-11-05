89607649018a5668f0c3d3a1e0a33875ea7e8460
Hasan Genc <hngenc@berkeley.edu>
2021-11-05 07:00:29 -0700

    512 l2 tlbs

--- src/main/scala/gemmini/CustomSoCConfigs.scala
+++ src/main/scala/gemmini/CustomSoCConfigs.scala
@@ -7,7 +7,7 @@ class CustomGemminiSoCConfig extends Config(
   new gemmini.GemminiCustomConfig ++
 
   // Set your custom L2 configs
-  new chipyard.config.WithL2TLBs(1024) ++
+  new chipyard.config.WithL2TLBs(512) ++
 
   new freechips.rocketchip.subsystem.WithInclusiveCache(
     nBanks = 1,
