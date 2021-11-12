f8f745f06ae4d2a1934ece695854c5599ee931b0
Hasan Genc <hngenc@berkeley.edu>
2021-11-11 16:24:00 -0800

    remove tutorial logs and  checkpoints

--- tutorial/run-onnx-inference.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /root/chipyard/generators/gemmini/software/onnxruntime-riscv/
-cd ./systolic_runner/imagenet_runner/
-
-spike --extension=gemmini pk ort_test -m resnet50_opt_quant.onnx -i images/dog.jpg  -p caffe2 -x 2 -O 99
-
