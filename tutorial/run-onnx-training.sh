2e6c3bb6a4b5ad49703a4d24b6153070f256c1b8
Hasan Genc <hngenc@berkeley.edu>
2022-08-28 13:28:49 -0700

    bump ort to include example trace

--- tutorial/run-onnx-training.sh
+++ tutorial/run-onnx-training.sh
@@ -3,4 +3,4 @@
 cd ./software/onnxruntime-riscv/
 cd ./systolic_runner/imagenet_trainer/
 
-spike --extension=gemmini pk resnet_train --model_name resnet50.onnx  --train_data_txt batch_out.txt --num_train_steps 1 --train_batch_size 2 -x 2 -d 0 -O 99
+spike --extension=gemmini pk resnet_train --model_name resnet50.onnx  --train_data_txt batch_out.txt --num_train_steps 1 --train_batch_size 2 -x 10 -d 0 -O 99
