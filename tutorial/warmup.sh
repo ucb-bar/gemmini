9655c1db41d6b54e2d57678a4e894436b134f0bc
Hasan Genc <hngenc@berkeley.edu>
2022-08-28 16:59:08 -0700

    fix warmup and bump ort

--- tutorial/warmup.sh
+++ tutorial/warmup.sh
@@ -7,7 +7,6 @@ grep -Ir DEADBEEF /root/builds/
 ./scripts/build-spike.sh
 ./tutorial/checkpoint.sh build-verilator
 ./tutorial/checkpoint.sh build-midas
-./tutorial/checkpoint.sh data-collection
 
 cd software/gemmini-rocc-tests
 ./build.sh
