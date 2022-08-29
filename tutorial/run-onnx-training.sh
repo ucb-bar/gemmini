35fcf577b83dfa3464f9c1cc19b9d3c8fcbb5313
Hasan Genc <hngenc@berkeley.edu>
2022-08-28 18:40:58 -0700

    dash

--- tutorial/run-onnx-training.sh
+++ tutorial/run-onnx-training.sh
@@ -3,5 +3,5 @@
 cd ./software/onnxruntime-riscv/
 cd ./systolic_runner/imagenet_trainer/
 
-spike --extension=gemmini pk resnet_train –model_name resnet50.onnx  --train_data_txt batch_out.txt --num_train_steps 1 --train_batch_size 10 -x 2 -d 0 -O 99
+spike --extension=gemmini pk resnet_train --model_name resnet50.onnx  --train_data_txt batch_out.txt --num_train_steps 1 --train_batch_size 10 -x 2 -d 0 -O 99
 
