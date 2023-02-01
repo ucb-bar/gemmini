534f8fc2569bb6e0aeb841abb76ae0d97e1eb3bf
Jerry Zhao <jerryz123@berkeley.edu>
2023-02-01 13:40:11 -0800

    Fix spike build in CI

--- .github/scripts/run-tests-spike.sh
+++ .github/scripts/run-tests-spike.sh
@@ -10,8 +10,12 @@ source $SCRIPT_DIR/enable-conda.sh
 cd $LOCAL_CHIPYARD_DIR
 source env.sh
 
-cd $LOCAL_CHIPYARD_DIR/toolchains/esp-tools/riscv-isa-sim/build
+git clone git@github.com:ucb-bar/esp-isa-sim.git
+cd esp-isa-sim
 git checkout $(cat $LOCAL_CHECKOUT_DIR/SPIKE.hash)
+mkdir build
+cd build
+../configure --prefix=$RISCV
 make && make install
 
 cd $LOCAL_CHIPYARD_DIR/generators/gemmini/software/gemmini-rocc-tests
