b6389f3ea7bbf070aa3dd40972daa5be7e2d4261
Jerry Zhao <jerryz123@berkeley.edu>
2023-02-05 09:24:18 -0800

    Remove esp-isa-sim install from CI

--- .github/scripts/run-tests-spike.sh
+++ .github/scripts/run-tests-spike.sh
@@ -10,14 +10,6 @@ source $SCRIPT_DIR/enable-conda.sh
 cd $LOCAL_CHIPYARD_DIR
 source env.sh
 
-git clone https://github.com/ucb-bar/esp-isa-sim.git
-cd esp-isa-sim
-git checkout $(cat $LOCAL_CHECKOUT_DIR/SPIKE.hash)
-mkdir build
-cd build
-../configure --prefix=$RISCV
-make && make install
-
 cd $LOCAL_CHIPYARD_DIR/generators/gemmini/software/gemmini-rocc-tests
 ./build.sh
 
