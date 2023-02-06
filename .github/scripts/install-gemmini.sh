8c33285c5a15aa60929e300b7ec766f4ef799c4e
Jerry Zhao <jerryz123@berkeley.edu>
2023-02-05 22:41:22 -0800

    Delete stale libgemmini before installing in CI

--- .github/scripts/install-gemmini.sh
+++ .github/scripts/install-gemmini.sh
@@ -33,6 +33,8 @@ git config --global --add safe.directory $LOCAL_CHECKOUT_DIR
 git config --global --add safe.directory '*'
 
 cd $LOCAL_CHECKOUT_DIR
+# Delete the stale libgemmini first installed by chipyard, switch to the one submoduled here
+rm -rf $RISCV/lib/libgemmini.so
 git submodule update --init software/libgemmini
 make -C software/libgemmini
 
