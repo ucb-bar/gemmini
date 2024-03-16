08cfde926f8a9cc9942795924cc8c19164b42134
SeahK <kimsea8209@berkeley.edu>
2024-03-16 00:42:49 -0700

    cut critical path

--- src/main/scala/gemmini/VectorScalarMultiplier.scala
+++ src/main/scala/gemmini/VectorScalarMultiplier.scala
@@ -198,6 +198,8 @@ object VectorScalarMultiplier {
   ) = {
     assert(!is_acc || is_mvin)
     val vsm = Module(new VectorScalarMultiplier(scale_args, cols, t, tag_t))
-    (vsm.io.req, vsm.io.resp)
+    val vsm_in_q = Module(new Queue(chiselTypeOf(vsm.io.req.bits), 2))
+    vsm.io.req <> vsm_in_q.io.deq
+    (vsm_in_q.io.enq, vsm.io.resp) 
   }
 }
