c83c1df1afb9989871308c4dc1f9805786d09052
Ruohan (Richard) Yan <richardyrh928@gmail.com>
2024-07-15 23:14:15 -0700

    dual port scratchpad tlram

--- src/main/scala/gemmini/Controller.scala
+++ src/main/scala/gemmini/Controller.scala
@@ -20,6 +20,273 @@ class GemminiCmd(rob_entries: Int)(implicit p: Parameters) extends Bundle {
   val from_conv_fsm = Bool()
 }
 
+class DPTLRAM(address: AddressSet, beatBytes: Int)
+             (implicit p: Parameters) extends LazyModule {
+  val r = TLManagerNode(Seq(
+    TLSlavePortParameters.v1(Seq(TLSlaveParameters.v2(
+      name = Some(s"spad_read_mgr"),
+      address = Seq(address),
+      supports = TLMasterToSlaveTransferSizes(
+        get = TransferSizes(1, beatBytes),
+      ),
+      fifoId = Some(0)
+    )), beatBytes = beatBytes)
+  ))
+
+  val w = TLManagerNode(Seq(
+    TLSlavePortParameters.v1(Seq(TLSlaveParameters.v2(
+      name = Some(s"spad_write_mgr"),
+      address = Seq(address),
+      supports = TLMasterToSlaveTransferSizes(
+        putFull = TransferSizes(1, beatBytes),
+        putPartial = TransferSizes(1, beatBytes),
+      ),
+      fifoId = Some(0)
+    )), beatBytes = beatBytes)
+  ))
+
+  lazy val module = new LazyModuleImp(this) {
+    def make_buffer[T <: Data](mem: TwoPortSyncMem[T], r_node: TLBundle, r_edge: TLEdgeIn,
+                               w_node: TLBundle, w_edge: TLEdgeIn): Unit = {
+      mem.io.ren := r_node.a.fire
+
+      val data_pipe_in = Wire(DecoupledIO(mem.io.rdata.cloneType))
+      data_pipe_in.valid := RegNext(mem.io.ren)
+      data_pipe_in.bits := mem.io.rdata
+
+      val metadata_pipe_in = Wire(DecoupledIO(new Bundle {
+        val source = r_node.a.bits.source.cloneType
+        val size = r_node.a.bits.size.cloneType
+      }))
+      metadata_pipe_in.valid := mem.io.ren
+      metadata_pipe_in.bits.source := r_node.a.bits.source
+      metadata_pipe_in.bits.size := r_node.a.bits.size
+
+      val sram_read_backup_reg = RegInit(0.U.asTypeOf(Valid(mem.io.rdata.cloneType)))
+
+      val data_pipe_inst = Module(new Pipeline(data_pipe_in.bits.cloneType, 1)())
+      data_pipe_inst.io.in <> data_pipe_in
+      val data_pipe = data_pipe_inst.io.out
+      val metadata_pipe = Pipeline(metadata_pipe_in, 2)
+      assert((data_pipe.valid || sram_read_backup_reg.valid) === metadata_pipe.valid)
+
+      // data pipe is filled, but D is not ready and SRAM read came back
+      when (data_pipe.valid && !r_node.d.ready && data_pipe_in.valid) {
+        assert(!data_pipe_in.ready) // we should fill backup reg only if data pipe is not enqueueing
+        assert(!sram_read_backup_reg.valid) // backup reg should be empty
+        assert(!metadata_pipe_in.ready) // metadata should be filled previous cycle
+        sram_read_backup_reg.valid := true.B
+        sram_read_backup_reg.bits := mem.io.rdata
+      }.otherwise {
+        assert(data_pipe_in.ready || !data_pipe_in.valid) // do not skip any response
+      }
+
+      assert(metadata_pipe_in.fire || !mem.io.ren) // when requesting sram, metadata needs to be ready
+      assert(r_node.d.fire === metadata_pipe.fire) // metadata dequeues iff D fires
+
+      // when D becomes ready, and data pipe has emptied, time for backup to empty
+      when (r_node.d.ready && sram_read_backup_reg.valid && !data_pipe.valid) {
+        sram_read_backup_reg.valid := false.B
+      }
+      // must empty backup before filling data pipe
+      assert(!(sram_read_backup_reg.valid && data_pipe.valid && data_pipe_in.fire))
+
+      r_node.d.bits := r_edge.AccessAck(
+        Mux(r_node.d.valid, metadata_pipe.bits.source, 0.U),
+        Mux(r_node.d.valid, metadata_pipe.bits.size, 0.U),
+        Mux(!data_pipe.valid, sram_read_backup_reg.bits, data_pipe.bits).asUInt)
+      r_node.d.valid := data_pipe.valid || sram_read_backup_reg.valid
+      // r node A is not ready only if D is not ready and both slots filled
+      r_node.a.ready := r_node.d.ready && !(data_pipe.valid && sram_read_backup_reg.valid)
+      data_pipe.ready := r_node.d.ready
+      metadata_pipe.ready := r_node.d.ready
+
+      // WRITE
+      mem.io.wen := RegNext(w_node.a.fire)
+      mem.io.wdata := RegNext(w_node.a.bits.data)
+      mem.io.mask := RegNext(VecInit(w_node.a.bits.mask.asBools))
+
+      val write_resp = Wire(Flipped(w_node.d.cloneType))
+      write_resp.bits := w_edge.AccessAck(w_node.a.bits)
+      write_resp.valid := w_node.a.valid
+      w_node.a.ready := write_resp.ready
+      w_node.d <> Queue(write_resp, 2)
+    }
+
+    val mem = TwoPortSyncMem(
+      n = ((address.mask + 1) / beatBytes).toInt,
+      t = UInt((beatBytes * 8).W),
+      mask_len = beatBytes
+    )
+
+    val (r_node, r_edge) = r.in.head
+    val (w_node, w_edge) = w.in.head
+
+    // address format is
+    // [ smem_base | bank_id | line_id | word_id | byte_offset ]
+    // line_id is used to index into the SRAMs
+    mem.io.raddr := (r_node.a.bits.address & address.mask.U) >> log2Ceil(beatBytes).U
+    mem.io.waddr := RegNext((w_node.a.bits.address & address.mask.U) >> log2Ceil(beatBytes).U)
+
+    make_buffer(mem, r_node, r_edge, w_node, w_edge)
+  }
+}
+
+object DPTLRAM {
+  def apply(address: AddressSet, beatBytes: Int)
+           (implicit p: Parameters): (TLManagerNode, TLManagerNode) = {
+    val dpram = LazyModule(new DPTLRAM(address, beatBytes))
+    (dpram.r, dpram.w)
+  }
+}
+
+class RWSplitterNode(visibility: Option[AddressSet], override val name: String = "rw_splitter")
+                    (implicit p: Parameters) extends LazyModule {
+  // this node accepts both read and write requests,
+  // splits & arbitrates them into one client node per type of operation;
+  // there will be N incoming edges, two outgoing edges, with two N:1 muxes;
+  // it keeps the read and write channels fully separate to allow parallel processing.
+  val node = TLNexusNode(
+    clientFn = { seq =>
+      val in_mapping = TLXbar.mapInputIds(seq)
+      val read_src_range = IdRange(in_mapping.map(_.start).min, in_mapping.map(_.end).max)
+      assert((read_src_range.start == 0) && isPow2(read_src_range.end))
+      val write_src_range = read_src_range.shift(read_src_range.size)
+      val visibilities = seq.flatMap(_.masters.flatMap(_.visibility))
+      val unified_vis = if (visibilities.map(_ == AddressSet.everything).reduce(_ || _)) Seq(AddressSet.everything)
+      else AddressSet.unify(visibilities)
+      println(s"$name has input visibilities $visibilities, unified to $unified_vis")
+
+      seq.head.v1copy(
+        echoFields = BundleField.union(seq.flatMap(_.echoFields)),
+        requestFields = BundleField.union(seq.flatMap(_.requestFields)),
+        responseKeys = seq.flatMap(_.responseKeys).distinct,
+        minLatency = seq.map(_.minLatency).min,
+        clients = Seq(
+          TLMasterParameters.v1(
+            name = s"${name}_read_client",
+            sourceId = read_src_range,
+            visibility = visibility.map(Seq(_)).getOrElse(unified_vis),
+            supportsProbe = TransferSizes.mincover(seq.map(_.anyEmitClaims.get)),
+            supportsGet = TransferSizes.mincover(seq.map(_.anyEmitClaims.get)),
+            supportsPutFull = TransferSizes.none,
+            supportsPutPartial = TransferSizes.none
+          ),
+          TLMasterParameters.v1(
+            name = s"${name}_write_client",
+            sourceId = write_src_range,
+            visibility = visibility.map(Seq(_)).getOrElse(unified_vis),
+            supportsProbe = TransferSizes.mincover(
+              seq.map(_.anyEmitClaims.putFull) ++seq.map(_.anyEmitClaims.putPartial)),
+            supportsGet = TransferSizes.none,
+            supportsPutFull = TransferSizes.mincover(seq.map(_.anyEmitClaims.putFull)),
+            supportsPutPartial = TransferSizes.mincover(seq.map(_.anyEmitClaims.putPartial))
+          )
+        )
+      )
+    },
+    managerFn = { seq =>
+      // val fifoIdFactory = TLXbar.relabeler()
+      println(f"combined address range of $name managers: " +
+        f"${AddressSet.unify(seq.flatMap(_.slaves.flatMap(_.address)))}, supports:" +
+        f"${seq.map(_.anySupportClaims).reduce(_ mincover _)}")
+
+      seq.head.v1copy(
+        responseFields = BundleField.union(seq.flatMap(_.responseFields)),
+        requestKeys = seq.flatMap(_.requestKeys).distinct,
+        minLatency = seq.map(_.minLatency).min,
+        endSinkId = TLXbar.mapOutputIds(seq).map(_.end).max,
+        managers = Seq(TLSlaveParameters.v2(
+          name = Some(s"${name}_manager"),
+          address = AddressSet.unify(seq.flatMap(_.slaves.flatMap(_.address))),
+          supports = seq.map(_.anySupportClaims).reduce(_ mincover _),
+          fifoId = Some(0),
+        ))
+      )
+    }
+  )
+
+  lazy val module = new LazyModuleImp(this) {
+    val u_out = node.out
+    val u_in = node.in
+    assert(u_out.length == 2, s"$name should have 2 outgoing edges but has ${u_out.length}")
+
+    val r_out = u_out.head
+    val w_out = u_out.last
+
+    val in_src = TLXbar.mapInputIds(u_in.map(_._2.client))
+    val in_src_size = in_src.map(_.end).max
+    assert(isPow2(in_src_size)) // should be checked already, but just to be sure
+
+    // arbitrate all reads into one read while assigning source prefix, same for write
+    val a_arbiter_in = (u_in zip in_src).map { case ((in_node, _), src_range) =>
+      val in_r: DecoupledIO[TLBundleA] =
+        WireDefault(0.U.asTypeOf(Decoupled(new TLBundleA(in_node.a.bits.params.copy(
+          sourceBits = log2Up(in_src_size) + 1
+        )))))
+      val in_w: DecoupledIO[TLBundleA] = WireDefault(0.U.asTypeOf(in_r.cloneType))
+
+      val req_is_read = in_node.a.bits.opcode === TLMessages.Get
+
+      (Seq(in_r.bits.user, in_r.bits.address, in_r.bits.opcode, in_r.bits.size,
+        in_r.bits.mask, in_r.bits.param, in_r.bits.data)
+        zip Seq(in_node.a.bits.user, in_node.a.bits.address, in_node.a.bits.opcode, in_node.a.bits.size,
+        in_node.a.bits.mask, in_node.a.bits.param, in_node.a.bits.data))
+        .foreach { case (x, y) => x := y }
+      in_r.bits.source := in_node.a.bits.source | src_range.start.U | Mux(req_is_read, 0.U, in_src_size.U)
+      in_w.bits := in_r.bits
+
+      in_r.valid := in_node.a.valid && req_is_read
+      in_w.valid := in_node.a.valid && !req_is_read
+      in_node.a.ready := Mux(req_is_read, in_r.ready, in_w.ready)
+
+      (in_r, in_w)
+    }
+    // we cannot use round robin because it might reorder requests, even from the same client
+    val (a_arbiter_in_r_nodes, a_arbiter_in_w_nodes) = a_arbiter_in.unzip
+    TLArbiter.lowest(r_out._2, r_out._1.a, a_arbiter_in_r_nodes:_*)
+    TLArbiter.lowest(w_out._2, w_out._1.a, a_arbiter_in_w_nodes:_*)
+
+    def trim(id: UInt, size: Int): UInt = if (size <= 1) 0.U else id(log2Ceil(size)-1, 0) // from Xbar
+    // for each unified mem node client, arbitrate read/write responses on d channel
+    (u_in zip in_src).zipWithIndex.foreach { case (((in_node, in_edge), src_range), i) =>
+      // assign d channel back based on source, invalid if source prefix mismatch
+      val resp = Seq(r_out._1.d, w_out._1.d)
+      val source_match = resp.zipWithIndex.map { case (r, i) =>
+        (r.bits.source(r.bits.source.getWidth - 1) === i.U(1.W)) && // MSB indicates read(0)/write(1)
+          src_range.contains(trim(r.bits.source, in_src_size))
+      }
+      val d_arbiter_in = resp.map(r => WireDefault(
+        0.U.asTypeOf(Decoupled(new TLBundleD(r.bits.params.copy(
+          sourceBits = in_node.d.bits.source.getWidth,
+          sizeBits = in_node.d.bits.size.getWidth
+        ))))
+      ))
+
+      (d_arbiter_in lazyZip resp lazyZip source_match).foreach { case (arb_in: DecoupledIO[TLBundleD], r, sm) =>
+        (Seq(arb_in.bits.user, arb_in.bits.opcode, arb_in.bits.data, arb_in.bits.param,
+          arb_in.bits.sink, arb_in.bits.denied, arb_in.bits.corrupt)
+          zip Seq(r.bits.user, r.bits.opcode, r.bits.data, r.bits.param,
+          r.bits.sink, r.bits.denied, r.bits.corrupt))
+          .foreach { case (x, y) => x := y }
+        arb_in.bits.source := trim(r.bits.source, 1 << in_node.d.bits.source.getWidth) // we can trim b/c isPow2(prefix)
+        arb_in.bits.size := trim(r.bits.size, 1 << in_node.d.bits.size.getWidth) // FIXME: check truncation
+
+        arb_in.valid := r.valid && sm
+        r.ready := arb_in.ready
+      }
+
+      TLArbiter.robin(in_edge, in_node.d, d_arbiter_in:_*)
+    }
+  }
+}
+
+object RWSplitterNode {
+  def apply()(implicit p: Parameters, valName: ValName): TLNexusNode = {
+    LazyModule(new RWSplitterNode(None, name = valName.name)).node
+  }
+}
+
 class Gemmini[T <: Data : Arithmetic, U <: Data, V <: Data](val config: GemminiArrayConfig[T, U, V])
                                      (implicit p: Parameters)
   extends LazyRoCC (
@@ -108,11 +375,21 @@ class Gemmini[T <: Data : Arithmetic, U <: Data, V <: Data](val config: GemminiA
   }) else TLIdentityNode()
 
   (0 until config.sp_banks).map { i =>
-    val ram = LazyModule(new TLRAM(
+    val (ram_r, ram_w) = DPTLRAM(
       address = AddressSet(spad_base + i * mem_width * mem_depth, mem_width * mem_depth - 1),
       beatBytes = mem_width,
-    ))
-    ram.node := TLFragmenter(32, 64) := TLBuffer() := xbar_node
+    )
+    val r_xbar = TLXbar(TLArbiter.lowestIndexFirst)
+    val w_xbar = TLXbar(TLArbiter.lowestIndexFirst)
+    r_xbar := spad_read_nodes
+    w_xbar := spad_write_nodes
+    val rw_splitter = RWSplitterNode()
+    rw_splitter := TLFragmenter(32, 64) := stlNode
+    r_xbar := rw_splitter
+    w_xbar := rw_splitter
+
+    ram_r := r_xbar
+    ram_w := w_xbar
   }
 
   // val acc_read_nodes = if (create_tl_mem) TLClientNode(Seq.tabulate(config.acc_banks) { i =>
@@ -122,17 +399,17 @@ class Gemmini[T <: Data : Arithmetic, U <: Data, V <: Data](val config: GemminiA
   //   TLMasterPortParameters.v1(Seq(TLMasterParameters.v1(name = s"acc_write_node_$i", sourceId = IdRange(0, numIDs))))
   // }) else TLIdentityNode()
 
-  spad.xbar_node :=* TLBuffer() :=* spad_read_nodes
-  spad.xbar_node :=* TLBuffer() :=* spad_write_nodes
+  // spad.xbar_node :=* TLBuffer() :=* spad_read_nodes
+  // spad.xbar_node :=* TLBuffer() :=* spad_write_nodes
 
-  spad_read_mgrs :*= TLBuffer() :*= xbar_node
-  spad_rw_mgrs :*= TLBuffer() :*= xbar_node
-  xbar_node := TLBuffer() := TLWidthWidget(config.dma_buswidth/8) := id_node
+  // spad_read_mgrs :*= TLBuffer() :*= xbar_node
+  // spad_rw_mgrs :*= TLBuffer() :*= xbar_node
+  // xbar_node := TLBuffer() := TLWidthWidget(config.dma_buswidth/8) := id_node
 
   override lazy val module = new GemminiModule(this)
   override val tlNode = if (config.use_dedicated_tl_port) spad.id_node else TLIdentityNode()
   override val atlNode = if (config.use_dedicated_tl_port) TLIdentityNode() else spad.id_node
-  id_node := stlNode
+  // id_node := stlNode
 
   val node = if (config.use_dedicated_tl_port) tlNode else atlNode
 }
