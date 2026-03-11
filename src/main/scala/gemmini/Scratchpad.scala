d2593a1bc1542e9061c0bc98a25332d507cee979
Rakanic <nicorakela@berkeley.edu>
2026-03-11 02:56:08 -0700

    fix address generation fp8

--- src/main/scala/gemmini/Scratchpad.scala
+++ src/main/scala/gemmini/Scratchpad.scala
@@ -64,12 +64,13 @@ class WriteReqExpander(local_addr_t: LocalAddr, acc_t_bits: Int, scale_t_bits: I
 
   val second_half = RegInit(false.B)
   val is_acc_write = io.in.bits.laddr.is_acc_addr && !io.in.bits.laddr.is_garbage()
+  val address_second_half  = io.in.bits.vaddr + (acc_t_bits/8).U * 16.U // 16 from 16 rows per tile, TODO (nicolas): make parametrizable
 
   io.out.valid := io.in.valid
   io.out.bits := io.in.bits
   io.out.bits.is_second_half := Mux(is_acc_write, second_half, false.B)
   io.out.bits.vaddr   := Mux(is_acc_write && second_half,
-    io.in.bits.vaddr + 4.U,
+    address_second_half,
     io.in.bits.vaddr)
 
   io.in.ready := io.out.ready && (!is_acc_write || second_half)
